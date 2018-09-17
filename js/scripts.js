b4acaf1991f334c2a59f37549b190ab48fb31a77
bhaskar-nair2 <b.bhaskar.nair@gmail.com>
2018-09-17 19:25:42 +0530

    About underline added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+$(document).ready(() => {
+    $('#mynav').css({
+        opacity: 1
+    })
+});
+
+$(window).on("load", function () {
+    $('#pre-loader').css({
+        display: 'None'
+    });
+
+});
 $(document).on('scroll', function (event) {
-    console.log($(document).scrollTop())
+    let dh = $(document).height();
+    let wh = $(window).height();
+    let scroll = $(document).scrollTop() / (dh - wh) * 10000;
+    console.log(scroll);
+
 });
